1cd318a94722887dda0f8f0fd28c1de5ce45dba6
Programming Hero <programming.hero1@gmail.com>
2025-06-18 17:18:23 -0500

    added merchant section background

--- src/pages/Home/BeMerchant/BeMerchant.jsx
+++ src/pages/Home/BeMerchant/BeMerchant.jsx
@@ -3,7 +3,7 @@ import location from '../../../assets/location-merchant.png'
 
 const BeMerchant = () => {
     return (
-        <div className="hero bg-[#03373D] rounded-4xl p-20">
+        <div data-aos="zoom-in-up" className="bg-[url('assets/be-a-merchant-bg.png')] bg-no-repeat bg-[#03373D] rounded-4xl p-20">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img
                     src={location}
