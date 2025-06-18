ca6c6ce23cb82ae6f57ae07060471562230687d5
Programming Hero <programming.hero1@gmail.com>
2025-06-18 16:44:49 -0500

    added be a merchant section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
 import ClientLogosMarquee from '../ClientLogosMarquee/ClientLogosMarquee';
 import Benefits from '../Benefits/Benefits';
+import BeMerchant from '../BeMerchant/BeMerchant';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Services></Services>
             <ClientLogosMarquee></ClientLogosMarquee>
             <Benefits></Benefits>
+            <BeMerchant></BeMerchant>
         </div>
     );
 };
