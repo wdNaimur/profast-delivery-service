330af630f15beeb97ab8d3c098257c1c1d2fa352
Programming Hero <programming.hero1@gmail.com>
2025-06-17 21:39:15 -0500

    navbar footer and layout setup

---
+++ src/pages/shared/ProFastLogo/ProFastLogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../../assets/logo.png'
+
+const ProFastLogo = () => {
+    return (
+        <div>
+            <img src={logo} alt="" />
+            <p>ProFast</p>
+        </div>
+    );
+};
+
+export default ProFastLogo;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,8 +1,16 @@
 import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: RootLayout,
+    children: [
+        {
+            index: true,
+            Component: Home
+        }
+    ]
   },
 ]);
