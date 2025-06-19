c048f3b3c8885eaeb975092178a515a5ce839476
Programming Hero <programming.hero1@gmail.com>
2025-06-18 23:01:20 -0500

    added simple react hook form for login

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+    }
+
     return (
         <div>
-            <h2>Thsi is Login</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <fieldset className="fieldset">
+
+                    <label className="label">Email</label>
+                    <input
+                        type="email"
+                        {...register('email')}
+                        className="input" placeholder="Email" />
+
+
+                    <label className="label">Password</label>
+                    <input
+                        type="password"
+                        {...register('password', {
+                            required: true,
+                            minLength: 6
+                        })}
+                        className="input" placeholder="Password" />
+
+                    <div><a className="link link-hover">Forgot password?</a></div>
+
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+            </form>
         </div>
     );
 };
