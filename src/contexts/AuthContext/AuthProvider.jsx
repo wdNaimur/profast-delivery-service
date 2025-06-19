afa26e889c8b93b044d5ca89a2c92f95bd68740f
Programming Hero <programming.hero1@gmail.com>
2025-06-19 09:35:37 -0500

    recap firebase auth and register user

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from '../../firebase/firebase.init';
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () =>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('user in the auth state change', currentUser)
+            setLoading(false);
+        });
+
+        return () => {
+            unSubscribe();
+        }
+    }, [])
 
     const authInfo = {
-
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut
     }
 
     return (
--- src/pages/Authentication/Register/Register.jsx
+++ src/pages/Authentication/Register/Register.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import useAuth from '../../../hooks/useAuth';
 
 const Register = () => {
-    const { register, handleSubmit, formState: {errors} } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { createUser } = useAuth();
 
     const onSubmit = data => {
         console.log(data);
+        createUser(data.email, data.password)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(error => {
+                console.error(error);
+            })
     }
 
     return (
@@ -17,15 +26,15 @@ const Register = () => {
                     <fieldset className="fieldset">
                         {/* email field */}
                         <label className="label">Email</label>
-                        <input type="email" 
-                        {...register('email', {required: true})} 
-                        className="input" placeholder="Email" />
+                        <input type="email"
+                            {...register('email', { required: true })}
+                            className="input" placeholder="Email" />
                         {
                             errors.email?.type === 'required' && <p className='text-red-500'>Email is required</p>
                         }
 
                         <label className="label">Password</label>
-                        <input type="password" {...register('password', {required: true, minLength: 6})} className="input" placeholder="Password" />
+                        <input type="password" {...register('password', { required: true, minLength: 6 })} className="input" placeholder="Password" />
                         {
                             errors.password?.type === 'required' && <p className='text-red-500'>Password is required</p>
                         }
