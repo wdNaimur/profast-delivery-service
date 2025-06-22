28b85027c81bdc2c5da39d337ebb9abf01b0856c
Programming Hero <programming.hero1@gmail.com>
2025-06-21 22:42:48 -0500

    Explore tanstack query and display parcels in table

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,19 @@ import { router } from './router/router.jsx';
 import 'aos/dist/aos.css';
 import Aos from 'aos';
 import AuthProvider from './contexts/AuthContext/AuthProvider.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 Aos.init();
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='font-urbanist max-w-7xl mx-auto'>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </QueryClientProvider>
     </div>
   </StrictMode>,
 )
